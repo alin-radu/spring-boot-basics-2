52ff8ce8a0ac545c84f5e2ddda8d72d3d0566097
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-29 20:02:36 +0200

    add project boilerplate

---
+++ src/main/java/com/dev/spring_boot_basics_3/SpringBootBasics3Application.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_basics_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasics3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasics3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/spring_boot_basics_3/SpringBootBasics3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_basics_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasics3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
