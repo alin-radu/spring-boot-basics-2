aeceb0bac6c2fd39666037d72df95fefdc2aaac2
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-30 19:07:53 +0200

    add spring-boot-starter-validation

--- src/main/java/com/dev/spring_boot_basics_3/controller/ContentController.java
+++ src/main/java/com/dev/spring_boot_basics_3/controller/ContentController.java
@@ -2,14 +2,26 @@ package com.dev.spring_boot_basics_3.controller;
 
 import com.dev.spring_boot_basics_3.model.Content;
 import com.dev.spring_boot_basics_3.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+/*
+ * @RestController
+ * Indicates that this class is a Spring MVC controller where
+ * every method returns a Java object that is automatically serialized to JSON.
+ */
+/*
+ * @RequestMapping
+ * Maps all requests that start with /api/content to this controller.
+ */
+
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 
     private final ContentCollectionRepository repository;
@@ -32,7 +44,7 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content) {
+    public void create(@Valid @RequestBody Content content) {
         repository.save(content);
     }
 
--- src/main/java/com/dev/spring_boot_basics_3/model/Content.java
+++ src/main/java/com/dev/spring_boot_basics_3/model/Content.java
@@ -1,11 +1,18 @@
 package com.dev.spring_boot_basics_3.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+
+        @NotBlank
         String title,
+
+        @NotBlank
         String desc,
+
         Status status,
         Type contentType,
         LocalDateTime dateCreated,
